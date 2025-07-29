c93c7970d337625db063304a0447a9c06b44e1d4
jagadeesh056 <jagadeeshsiddani01@gmail.com>
2025-07-29 19:57:01 +0530

    output_parsers

---
+++ Outputparsers/StrOutputParser1.py
@@ -0,0 +1,31 @@
+from langchain_huggingface import HuggingFaceEndpoint, ChatHuggingFace
+from langchain_core.prompts import PromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+from dotenv import load_dotenv
+
+load_dotenv()
+
+llm = HuggingFaceEndpoint(
+    repo_id="google/gemma-2-2b-it",
+    task="text-generation"
+)
+
+model = ChatHuggingFace(llm = llm)
+
+template1 = PromptTemplate(
+    template="Write detailed report on {topic}",
+    input_variables=['topic']
+)
+
+template2 = PromptTemplate(
+    template='Write 5 lines summary on {text}',
+    input_variables=['text']
+)
+
+parser = StrOutputParser()
+
+chain = template1 | model | parser | template2 | model | parser
+
+result = chain.invoke({'topic': 'milky-way galaxy'})
+
+print(result)
