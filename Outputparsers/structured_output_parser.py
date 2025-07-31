59c8f015063bad4f8efeabdd9feb81c33e678c9a
jagadeesh056 <jagadeeshsiddani01@gmail.com>
2025-07-31 11:34:29 +0530

    output-parsers

---
+++ Outputparsers/structured_output_parser.py
@@ -0,0 +1,29 @@
+from langchain_huggingface import ChatHuggingFace,HuggingFaceEndpoint
+from langchain.output_parsers import StructuredOutputParser, ResponseSchema
+from langchain_core.prompts import PromptTemplate
+from dotenv import load_dotenv
+
+load_dotenv()
+
+llm = HuggingFaceEndpoint(
+    repo_id="google/gemma-2-2b-it",
+    task="text-generation"
+)
+
+model = ChatHuggingFace(llm = llm)
+
+#schema
+schema = [
+    ResponseSchema(name= 'fact_1', description='Fact 1 about the topic'),
+    ResponseSchema(name='fact_2', description='Fact 2 about the topic'),
+    ResponseSchema(name='fact_3', description='Fact 3 about the topic')
+]
+
+parser = StructuredOutputParser.from_response_schemas(schema)
+
+template = PromptTemplate(
+    template='Give me 3 facts about {topic} /n {format_instruction}',
+    input_variables=['topic'],
+    partial_variables={'format_instruction': parser.get_format_instructions()}
+)
+
